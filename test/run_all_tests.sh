5b5088957aca3af94da7dfbf689e4949e9479de0
stryku <stryku2393@gmail.com>
2020-12-04 23:48:51 +0100

    Added print at the end

--- test/run_all_tests.sh
+++ test/run_all_tests.sh
@@ -4,6 +4,8 @@ source test_common.sh
 
 MODULE_NAME=$1
 
+FAILED_TESTS_COUNT=0
+
 for f in *.out
 do
     test_log "Testing ${f}..."
@@ -20,6 +22,7 @@ do
         test_log "Testing ${f}... FAILED"
         rm -r ${DDFS_DIR}
         rm ${DDFS_IMG}
+        FAILED_TESTS_COUNT=$((${FAILED_TESTS_COUNT}+1))
         continue
     fi
 
@@ -30,6 +33,7 @@ do
         test_log "Testing ${f}... PASSED"
     else
         test_log "Testing ${f}... FAILED"
+        FAILED_TESTS_COUNT=$((${FAILED_TESTS_COUNT}+1))
     fi
 
     ./test_teardown.sh $MODULE_NAME $DDFS_DIR
@@ -39,8 +43,16 @@ do
         test_log "Testing ${f}... TEST TEARDOWN SUCCESS"
     else
         test_log "Testing ${f}... FAILED"
+        FAILED_TESTS_COUNT=$((${FAILED_TESTS_COUNT}+1))
     fi
 
     rm -r ${DDFS_DIR}
     rm ${DDFS_IMG}
 done
+
+if [[ ${FAILED_TESTS_COUNT} -eq 0 ]]
+then
+    test_log "ALL TESTS PASSED"
+else
+    test_log "SOME TESTS FAILED"
+fi
