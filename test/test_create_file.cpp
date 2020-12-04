4d01ba5860f7edebee1d66694b3353d1525a66b9
stryku <stryku2393@gmail.com>
2020-12-04 23:32:03 +0100

    Introduced common.hpp

--- test/test_create_file.cpp
+++ test/test_create_file.cpp
@@ -3,9 +3,11 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "3rdparty/doctest.h"
 
+#include "common.hpp"
+
 TEST_CASE("DDFS.FileCreation")
 {
-	const std::string file_path = DDFS_MOUNTED_DIR + std::string{ "/aaa" };
+	const std::string file_path = make_path("/aaa");
 	FILE *fp = std::fopen(file_path.c_str(), "w");
 	REQUIRE(fp != nullptr);
 
