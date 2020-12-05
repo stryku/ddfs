68516ffd41ba9877a759679164f5dd20cdd73ccc
stryku <stryku2393@gmail.com>
2020-12-05 21:11:18 +0100

    Added test DDFS.DirEntry.calc_dir_entry_offsets.40 entry, far cluster, mixed blocks

--- test/unit/test_dir_entry.cpp
+++ test/unit/test_dir_entry.cpp
@@ -5,117 +5,171 @@
 
 #include <dir_entry.h>
 
-TEST_CASE("DDFS.DirEntry.calc_dir_entry_offsets.block 0")
+TEST_CASE(
+	"DDFS.DirEntry.calc_dir_entry_offsets.first entry, first cluster, first block")
 {
-	{
-		const auto calc_params =
-			ddfs_dir_entry_calc_params{ .entries_per_cluster = 10,
-						    .blocks_per_cluster = 1,
-						    .data_cluster_no = 0,
-						    .block_size = 512,
-						    .dir_logical_start = 0 };
+	const auto calc_params =
+		ddfs_dir_entry_calc_params{ .entries_per_cluster = 10,
+					    .blocks_per_cluster = 1,
+					    .data_cluster_no = 0,
+					    .block_size = 512,
+					    .dir_logical_start = 0 };
 
-		const auto result =
-			ddfs_calc_dir_entry_offsets(&calc_params, 0);
+	const auto result = ddfs_calc_dir_entry_offsets(&calc_params, 0);
 
-		REQUIRE_EQ(result.name.block_on_device, 0);
-		REQUIRE_EQ(result.name.offset_on_block, 0);
+	REQUIRE_EQ(result.name.block_on_device, 0);
+	REQUIRE_EQ(result.name.offset_on_block, 0);
 
-		REQUIRE_EQ(result.attributes.block_on_device, 0);
-		REQUIRE_EQ(result.attributes.offset_on_block, 10 * 4);
+	REQUIRE_EQ(result.attributes.block_on_device, 0);
+	REQUIRE_EQ(result.attributes.offset_on_block, 10 * 4);
 
-		REQUIRE_EQ(result.size.block_on_device, 0);
-		REQUIRE_EQ(result.size.offset_on_block,
-			   10 * (4 + sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE)));
+	REQUIRE_EQ(result.size.block_on_device, 0);
+	REQUIRE_EQ(result.size.offset_on_block,
+		   10 * (4 + sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE)));
 
-		REQUIRE_EQ(result.first_cluster.block_on_device, 0);
-		REQUIRE_EQ(result.first_cluster.offset_on_block,
-			   10 * (4 + sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE) +
-				 sizeof(DDFS_DIR_ENTRY_SIZE_TYPE)));
-	}
-	{
-		const auto calc_params =
-			ddfs_dir_entry_calc_params{ .entries_per_cluster = 10,
-						    .blocks_per_cluster = 1,
-						    .data_cluster_no = 0,
-						    .block_size = 512,
-						    .dir_logical_start = 0 };
+	REQUIRE_EQ(result.first_cluster.block_on_device, 0);
+	REQUIRE_EQ(result.first_cluster.offset_on_block,
+		   10 * (4 + sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE) +
+			 sizeof(DDFS_DIR_ENTRY_SIZE_TYPE)));
+}
 
-		const auto result =
-			ddfs_calc_dir_entry_offsets(&calc_params, 3);
+TEST_CASE(
+	"DDFS.DirEntry.calc_dir_entry_offsets.3 entry, first cluster, first block")
+{
+	const auto calc_params =
+		ddfs_dir_entry_calc_params{ .entries_per_cluster = 10,
+					    .blocks_per_cluster = 1,
+					    .data_cluster_no = 0,
+					    .block_size = 512,
+					    .dir_logical_start = 0 };
 
-		REQUIRE_EQ(result.name.block_on_device, 0);
-		REQUIRE_EQ(result.name.offset_on_block,
-			   3 * DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE);
+	const auto result = ddfs_calc_dir_entry_offsets(&calc_params, 3);
 
-		REQUIRE_EQ(result.attributes.block_on_device, 0);
-		REQUIRE_EQ(result.attributes.offset_on_block,
-			   10 * 4 + 3 * sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE));
+	REQUIRE_EQ(result.name.block_on_device, 0);
+	REQUIRE_EQ(result.name.offset_on_block,
+		   3 * DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE);
 
-		REQUIRE_EQ(result.size.block_on_device, 0);
-		REQUIRE_EQ(result.size.offset_on_block,
-			   10 * (4 + sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE)) +
-				   3 * sizeof(DDFS_DIR_ENTRY_SIZE_TYPE));
+	REQUIRE_EQ(result.attributes.block_on_device, 0);
+	REQUIRE_EQ(result.attributes.offset_on_block,
+		   10 * 4 + 3 * sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE));
 
-		REQUIRE_EQ(result.first_cluster.block_on_device, 0);
-		REQUIRE_EQ(
-			result.first_cluster.offset_on_block,
-			10 * (4 + sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE) +
-			      sizeof(DDFS_DIR_ENTRY_SIZE_TYPE)) +
-				3 * sizeof(DDFS_DIR_ENTRY_FIRST_CLUSTER_TYPE));
-	}
-	{
-		const auto calc_params =
-			ddfs_dir_entry_calc_params{ .entries_per_cluster = 30,
-						    .blocks_per_cluster = 2,
-						    .data_cluster_no = 5,
-						    .block_size = 256,
-						    .dir_logical_start = 10 };
+	REQUIRE_EQ(result.size.block_on_device, 0);
+	REQUIRE_EQ(result.size.offset_on_block,
+		   10 * (4 + sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE)) +
+			   3 * sizeof(DDFS_DIR_ENTRY_SIZE_TYPE));
 
-		const auto cluster_first_block_on_device =
-			calc_params.blocks_per_cluster *
-			(calc_params.data_cluster_no +
-			 calc_params.dir_logical_start);
+	REQUIRE_EQ(result.first_cluster.block_on_device, 0);
+	REQUIRE_EQ(result.first_cluster.offset_on_block,
+		   10 * (4 + sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE) +
+			 sizeof(DDFS_DIR_ENTRY_SIZE_TYPE)) +
+			   3 * sizeof(DDFS_DIR_ENTRY_FIRST_CLUSTER_TYPE));
+}
 
-		const auto result =
-			ddfs_calc_dir_entry_offsets(&calc_params, 7);
+TEST_CASE(
+	"DDFS.DirEntry.calc_dir_entry_offsets.7 entry, first cluster, mixed blocks")
+{
+	const auto calc_params =
+		ddfs_dir_entry_calc_params{ .entries_per_cluster = 30,
+					    .blocks_per_cluster = 2,
+					    .data_cluster_no = 5,
+					    .block_size = 256,
+					    .dir_logical_start = 10 };
 
-		// =28 -> first block on cluster
-		const auto expected_name_offset = 7 * 4;
-		REQUIRE_EQ(result.name.block_on_device,
-			   cluster_first_block_on_device);
-		REQUIRE_EQ(result.name.offset_on_block, expected_name_offset);
+	const auto cluster_first_block_on_device =
+		calc_params.blocks_per_cluster *
+		(calc_params.data_cluster_no + calc_params.dir_logical_start);
 
-		// =127 -> first block on cluster
-		const auto expected_atr_offset =
-			calc_params.entries_per_cluster *
-				DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE +
-			7 * sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE);
-		REQUIRE_EQ(result.attributes.block_on_device,
-			   cluster_first_block_on_device);
-		REQUIRE_EQ(result.attributes.offset_on_block,
-			   expected_atr_offset);
+	const auto result = ddfs_calc_dir_entry_offsets(&calc_params, 7);
 
-		// =206 -> second block on cluster
-		const auto expected_size_offset =
-			calc_params.entries_per_cluster *
-				(DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE +
-				 sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE)) +
-			7 * sizeof(DDFS_DIR_ENTRY_SIZE_TYPE);
-		REQUIRE_EQ(result.size.block_on_device,
-			   cluster_first_block_on_device + 1);
-		REQUIRE_EQ(result.size.offset_on_block, expected_size_offset);
+	// =28 -> first block on cluster
+	const auto expected_name_offset = 7 * 4;
+	REQUIRE_EQ(result.name.block_on_device, cluster_first_block_on_device);
+	REQUIRE_EQ(result.name.offset_on_block, expected_name_offset);
 
-		// =418 -> second block on cluster
-		const auto expected_first_cluster_offset =
-			calc_params.entries_per_cluster *
-				(DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE +
-				 sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE) +
-				 sizeof(DDFS_DIR_ENTRY_SIZE_TYPE)) +
-			7 * sizeof(DDFS_DIR_ENTRY_FIRST_CLUSTER_TYPE);
-		REQUIRE_EQ(result.first_cluster.block_on_device,
-			   cluster_first_block_on_device + 1);
-		REQUIRE_EQ(result.first_cluster.offset_on_block,
-			   expected_first_cluster_offset);
-	}
+	// =127 -> first block on cluster
+	const auto expected_atr_offset =
+		calc_params.entries_per_cluster *
+			DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE +
+		7 * sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE);
+	REQUIRE_EQ(result.attributes.block_on_device,
+		   cluster_first_block_on_device);
+	REQUIRE_EQ(result.attributes.offset_on_block, expected_atr_offset);
+
+	// =206 -> first block on cluster
+	const auto expected_size_offset =
+		calc_params.entries_per_cluster *
+			(DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE +
+			 sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE)) +
+		7 * sizeof(DDFS_DIR_ENTRY_SIZE_TYPE);
+	REQUIRE_EQ(result.size.block_on_device, cluster_first_block_on_device);
+	REQUIRE_EQ(result.size.offset_on_block, expected_size_offset);
+
+	// =418 -> second block on cluster, thus offset on block = 418 - 256
+	const auto expected_first_cluster_offset =
+		(calc_params.entries_per_cluster *
+			 (DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE +
+			  sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE) +
+			  sizeof(DDFS_DIR_ENTRY_SIZE_TYPE)) +
+		 7 * sizeof(DDFS_DIR_ENTRY_FIRST_CLUSTER_TYPE)) -
+		256;
+	REQUIRE_EQ(result.first_cluster.block_on_device,
+		   cluster_first_block_on_device + 1);
+	REQUIRE_EQ(result.first_cluster.offset_on_block,
+		   expected_first_cluster_offset);
+}
+
+TEST_CASE(
+	"DDFS.DirEntry.calc_dir_entry_offsets.40 entry, far cluster, mixed blocks")
+{
+	const auto calc_params =
+		ddfs_dir_entry_calc_params{ .entries_per_cluster = 30,
+					    .blocks_per_cluster = 2,
+					    .data_cluster_no = 5,
+					    .block_size = 256,
+					    .dir_logical_start = 10 };
+
+	const auto expected_entry_cluster =
+		calc_params.data_cluster_no + calc_params.dir_logical_start + 1;
+	const auto cluster_first_block_on_device =
+		calc_params.blocks_per_cluster * expected_entry_cluster;
+
+	const auto entry = 40;
+	const auto entry_on_cluster = entry - calc_params.entries_per_cluster;
+	const auto result = ddfs_calc_dir_entry_offsets(&calc_params, entry);
+
+	// =28 -> first block on cluster
+	const auto expected_name_offset = entry_on_cluster * 4;
+	REQUIRE_EQ(result.name.block_on_device, cluster_first_block_on_device);
+	REQUIRE_EQ(result.name.offset_on_block, expected_name_offset);
+
+	// =127 -> first block on cluster
+	const auto expected_atr_offset =
+		calc_params.entries_per_cluster *
+			DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE +
+		entry_on_cluster * sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE);
+	REQUIRE_EQ(result.attributes.block_on_device,
+		   cluster_first_block_on_device);
+	REQUIRE_EQ(result.attributes.offset_on_block, expected_atr_offset);
+
+	// =206 -> first block on cluster
+	const auto expected_size_offset =
+		calc_params.entries_per_cluster *
+			(DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE +
+			 sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE)) +
+		entry_on_cluster * sizeof(DDFS_DIR_ENTRY_SIZE_TYPE);
+	REQUIRE_EQ(result.size.block_on_device, cluster_first_block_on_device);
+	REQUIRE_EQ(result.size.offset_on_block, expected_size_offset);
+
+	// =418 -> second block on cluster, thus offset on block = 418 - 256
+	const auto expected_first_cluster_offset =
+		(calc_params.entries_per_cluster *
+			 (DDFS_DIR_ENTRY_NAME_CHARS_IN_PLACE +
+			  sizeof(DDFS_DIR_ENTRY_ATTRIBUTES_TYPE) +
+			  sizeof(DDFS_DIR_ENTRY_SIZE_TYPE)) +
+		 entry_on_cluster * sizeof(DDFS_DIR_ENTRY_FIRST_CLUSTER_TYPE)) -
+		256;
+	REQUIRE_EQ(result.first_cluster.block_on_device,
+		   cluster_first_block_on_device + 1);
+	REQUIRE_EQ(result.first_cluster.offset_on_block,
+		   expected_first_cluster_offset);
 }
