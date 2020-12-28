9f08f94b8042906f9d6f588ccd8ec5dd327e1690
stryku <stryku2393@gmail.com>
2020-12-28 19:23:23 +0100

    Removed commented out code

--- module/ddfs.c
+++ module/ddfs.c
@@ -109,20 +109,7 @@ static int __ddfs_write_inode(struct inode *inode, int wait)
 	// Todo check whether entry index is inside cluster
 
 	{
-		/*
-		block_provider block_providing_fun,
-			void *block_providing_data,
-			const struct ddfs_dir_entry_calc_params *calc_params,
-			unsigned entry_index, unsigned part_flags
-		*/
 		const unsigned part_flags = DDFS_PART_ALL;
-		/*
-	unsigned entries_per_cluster;
-	unsigned blocks_per_cluster;
-	unsigned data_cluster_no;
-	unsigned block_size;
-	unsigned dir_logical_start
-*/
 
 		const struct ddfs_dir_entry_calc_params calc_params = {
 			.entries_per_cluster = sbi->entries_per_cluster,
@@ -135,9 +122,6 @@ static int __ddfs_write_inode(struct inode *inode, int wait)
 		struct dir_entry_ptrs entry_ptrs = ddfs_access_dir_entries(
 			default_block_reading_provider, sb, &calc_params,
 			dd_inode->dentry_index, part_flags);
-
-		// struct dir_entry_ptrs entry_ptrs = ddfs_access_dir_entries(
-		// 	inode, dd_inode->dentry_index, part_flags);
 
 		*entry_ptrs.first_cluster.ptr = dd_inode->i_logstart;
 		*entry_ptrs.size.ptr = inode->i_size;
@@ -363,6 +347,7 @@ ddfs_make_dir_entry(const struct dir_entry_ptrs *parts_ptrs)
 static long ddfs_add_dir_entry(struct inode *dir, const struct qstr *qname,
 			       struct ddfs_dir_entry *de)
 {
+	int i;
 	struct ddfs_inode_info *dd_idir = DDFS_I(dir);
 	struct super_block *sb = dir->i_sb;
 	struct ddfs_sb_info *sbi = DDFS_SB(sb);
@@ -381,10 +366,6 @@ static long ddfs_add_dir_entry(struct inode *dir, const struct qstr *qname,
 	const struct dir_entry_ptrs parts_ptrs = ddfs_access_dir_entries(
 		default_block_reading_provider, sb, &calc_params,
 		new_entry_index, DDFS_PART_NAME | DDFS_PART_FIRST_CLUSTER);
-
-	// const struct dir_entry_ptrs parts_ptrs = access_dir_entries(
-	// 	dir, new_entry_index, DDFS_PART_NAME | DDFS_PART_FIRST_CLUSTER);
-	int i;
 
 	dd_print("ddfs_add_dir_entry, dir: %p, name: %s, de: %p", dir,
 		 (const char *)qname->name, de);
@@ -830,10 +811,6 @@ static int ddfs_find(struct inode *dir, const char *name,
 			ddfs_access_dir_entries(default_block_reading_provider,
 						sb, &calc_params, entry_index,
 						DDFS_PART_ALL);
-
-		// const struct dir_entry_ptrs entry_ptrs =
-		// 	ddfs_access_dir_entries(dir, entry_index,
-		// 				DDFS_PART_ALL);
 
 		dd_print("entry_index: %d", entry_index);
 		dump_dir_entry_ptrs(&entry_ptrs);
