20951c6ab5cc2c222eb6b2fafa28c1f2c5fc828f
stryku <stryku2393@gmail.com>
2020-12-30 12:51:40 +0100

    Introduced sbi_values

--- module/ddfs.h
+++ module/ddfs.h
@@ -14,6 +14,7 @@
 #include <linux/writeback.h>
 
 #include "dir_entry.h"
+#include "sbi_values.h"
 
 /*
  * DDFS inode data in memory
@@ -70,26 +71,7 @@ static inline struct ddfs_inode_info *DDFS_I(struct inode *inode)
 }
 
 struct ddfs_sb_info {
-	unsigned int table_offset; // From begin of partition
-	unsigned int table_size; // In bytes
-	unsigned int number_of_table_entries;
-
-	unsigned int cluster_size; // In bytes
-
-	unsigned int data_offset; // From begin of partition
-	unsigned int data_cluster_no; // Data first cluster no
-
-	unsigned int root_cluster; //First cluster of root dir.
-
-	unsigned long block_size; // Size of block (sector) in bytes
-	unsigned int blocks_per_cluster; // Number of blocks (sectors) per cluster
-
-	unsigned int entries_per_cluster; // Dir entries per cluster
-
-	unsigned int name_entries_offset;
-	unsigned int attributes_entries_offset;
-	unsigned int size_entries_offset;
-	unsigned int first_cluster_entries_offset;
+	struct ddfs_sbi_values v;
 
 	struct mutex table_lock;
 	struct mutex s_lock;
