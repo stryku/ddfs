cf67c557cbb8be58c734eb99578f4c9c74c93ab6
stryku <stryku2393@gmail.com>
2020-12-31 10:06:05 +0100

    Don't use designated initializer

--- test/unit/test_table.cpp
+++ test/unit/test_table.cpp
@@ -13,9 +13,12 @@ TEST_CASE("DDFS.Table.find_free_cluster empty table")
 {
 	std::vector<DDFS_TABLE_ENTRY_TYPE> clusters(k_entries_per_cluster,
 						    DDFS_CLUSTER_UNUSED);
-	ddfs_table table{ .clusters = clusters.data(), .index_offset = 0u };
-	ddfs_sbi_values sbi_v{ .number_of_table_entries_per_cluster =
-				       k_entries_per_cluster };
+	ddfs_table table;
+	table.clusters = clusters.data();
+	table.index_offset = 0u;
+
+	ddfs_sbi_values sbi_v;
+	sbi_v.number_of_table_entries_per_cluster = k_entries_per_cluster;
 
 	const auto result = ddfs_table_find_free_cluster(&table, &sbi_v);
 
@@ -26,9 +29,12 @@ TEST_CASE("DDFS.Table.find_free_cluster almost all taken")
 {
 	std::vector<DDFS_TABLE_ENTRY_TYPE> clusters(k_entries_per_cluster,
 						    DDFS_CLUSTER_EOF);
-	ddfs_table table{ .clusters = clusters.data(), .index_offset = 0u };
-	ddfs_sbi_values sbi_v{ .number_of_table_entries_per_cluster =
-				       k_entries_per_cluster };
+	ddfs_table table;
+	table.clusters = clusters.data();
+	table.index_offset = 0u;
+
+	ddfs_sbi_values sbi_v;
+	sbi_v.number_of_table_entries_per_cluster = k_entries_per_cluster;
 
 	clusters[16] = DDFS_CLUSTER_UNUSED;
 	clusters[32] = DDFS_CLUSTER_UNUSED;
@@ -52,9 +58,12 @@ TEST_CASE("DDFS.Table.find_free_cluster all taken")
 {
 	std::vector<DDFS_TABLE_ENTRY_TYPE> clusters(k_entries_per_cluster,
 						    DDFS_CLUSTER_EOF);
-	ddfs_table table{ .clusters = clusters.data(), .index_offset = 0u };
-	ddfs_sbi_values sbi_v{ .number_of_table_entries_per_cluster =
-				       k_entries_per_cluster };
+	ddfs_table table;
+	table.clusters = clusters.data();
+	table.index_offset = 0u;
+
+	ddfs_sbi_values sbi_v;
+	sbi_v.number_of_table_entries_per_cluster = k_entries_per_cluster;
 
 	const auto result = ddfs_table_find_free_cluster(&table, &sbi_v);
 	REQUIRE_EQ(result, -1);
